71d2def8f2a59f712e6cb64dbf16fc22d4603d4c
Jhonatan Alves <jhon_fla@hotmail.com>
2015-03-25 20:49:36 -0300

    Começando a identação e substituindo 'tabs' por 4 espaços conforme a folha de estilo

--- games/tetris/input.c
+++ games/tetris/input.c
@@ -50,16 +50,16 @@
 
 /* return true iff the given timeval is positive */
 #define	TV_POS(tv) \
-	((tv)->tv_sec > 0 || ((tv)->tv_sec == 0 && (tv)->tv_usec > 0))
+    ((tv)->tv_sec > 0 || ((tv)->tv_sec == 0 && (tv)->tv_usec > 0))
 
 /* subtract timeval `sub' from `res' */
 #define	TV_SUB(res, sub) \
-	(res)->tv_sec -= (sub)->tv_sec; \
-	(res)->tv_usec -= (sub)->tv_usec; \
-	if ((res)->tv_usec < 0) { \
-		(res)->tv_usec += 1000000; \
-		(res)->tv_sec--; \
-	}
+    (res)->tv_sec -= (sub)->tv_sec; \
+    (res)->tv_usec -= (sub)->tv_usec; \
+    if ((res)->tv_usec < 0) { \
+        (res)->tv_usec += 1000000; \
+        (res)->tv_sec--; \
+    }
 
 /*
  * Do a `read wait': poll for reading from stdin, with timeout *tvp.
@@ -71,92 +71,88 @@
  *
  * Return 0 => no input, 1 => can read() from stdin
  */
-int
-rwait(struct timeval *tvp)
-{
-	struct pollfd set[1];
-	struct timeval starttv, endtv;
-	int timeout;
+
+int rwait(struct timeval *tvp) {
+    struct pollfd set[1];
+    struct timeval starttv, endtv;
+    int timeout;
+
 #define	NILTZ ((struct timezone *)0)
 
-	if (tvp) {
-		(void) gettimeofday(&starttv, NILTZ);
-		endtv = *tvp;
-		timeout = tvp->tv_sec * 1000 + tvp->tv_usec / 1000;
-	} else
-		timeout = INFTIM;
+    if (tvp) {
+        (void) gettimeofday(&starttv, NILTZ);
+        endtv = *tvp;
+        timeout = tvp->tv_sec * 1000 + tvp->tv_usec / 1000;
+    } else
+        timeout = INFTIM;
 again:
-	set[0].fd = STDIN_FILENO;
-	set[0].events = POLLIN;
-	switch (poll(set, 1, timeout)) {
+    set[0].fd = STDIN_FILENO;
+    set[0].events = POLLIN;
+    switch (poll(set, 1, timeout)) {
 
-	case -1:
-		if (tvp == 0)
-			return (-1);
-		if (errno == EINTR)
-			goto again;
-		stop("poll failed, help");
-		/* NOTREACHED */
+    case -1:
+        if (tvp == 0)
+            return (-1);
+        if (errno == EINTR)
+            goto again;
+        stop("poll failed, help");
+        /* NOTREACHED */
 
-	case 0:	/* timed out */
-		if (tvp) {
-			tvp->tv_sec = 0;
-			tvp->tv_usec = 0;
-		}
-		return (0);
-	}
-	if (tvp) {
-		/* since there is input, we may not have timed out */
-		(void) gettimeofday(&endtv, NILTZ);
-		TV_SUB(&endtv, &starttv);
-		TV_SUB(tvp, &endtv);	/* adjust *tvp by elapsed time */
-	}
-	return (1);
+    case 0:	/* timed out */
+        if (tvp) {
+            tvp->tv_sec = 0;
+            tvp->tv_usec = 0;
+        }
+        return (0);
+    }
+    if (tvp) {
+        /* since there is input, we may not have timed out */
+        (void) gettimeofday(&endtv, NILTZ);
+        TV_SUB(&endtv, &starttv);
+        TV_SUB(tvp, &endtv);	/* adjust *tvp by elapsed time */
+    }
+    return (1);
 }
 
 /*
  * `sleep' for the current turn time.
  * Eat any input that might be available.
  */
-void
-tsleep(void)
-{
-	struct timeval tv;
-	char c;
+void tsleep(void) {
+    struct timeval tv;
+    char c;
 
-	tv.tv_sec = 0;
-	tv.tv_usec = fallrate;
-	while (TV_POS(&tv))
-		if (rwait(&tv) && read(0, &c, 1) != 1)
-			break;
+    tv.tv_sec = 0;
+    tv.tv_usec = fallrate;
+    while (TV_POS(&tv))
+        if (rwait(&tv) && read(0, &c, 1) != 1)
+            break;
 }
 
 /*
  * getchar with timeout.
  */
-int
-tgetchar(void)
-{
-	static struct timeval timeleft;
-	char c;
+int tgetchar(void) {
+    static struct timeval timeleft;
+    char c;
 
-	/*
-	 * Reset timeleft to fallrate whenever it is not positive.
-	 * In any case, wait to see if there is any input.  If so,
-	 * take it, and update timeleft so that the next call to
-	 * tgetchar() will not wait as long.  If there is no input,
-	 * make timeleft zero or negative, and return -1.
-	 *
-	 * Most of the hard work is done by rwait().
-	 */
-	if (!TV_POS(&timeleft)) {
-		faster();	/* go faster */
-		timeleft.tv_sec = 0;
-		timeleft.tv_usec = fallrate;
-	}
-	if (!rwait(&timeleft))
-		return (-1);
-	if (read(0, &c, 1) != 1)
-		stop("end of file, help");
-	return ((int)(unsigned char)c);
+    /*
+     * Reset timeleft to fallrate whenever it is not positive.
+     * In any case, wait to see if there is any input.  If so,
+     * take it, and update timeleft so that the next call to
+     * tgetchar() will not wait as long.  If there is no input,
+     * make timeleft zero or negative, and return -1.
+     *
+     * Most of the hard work is done by rwait().
+     */
+    if (!TV_POS(&timeleft)) {
+        faster();	/* go faster */
+        timeleft.tv_sec = 0;
+        timeleft.tv_usec = fallrate;
+    }
+    if (!rwait(&timeleft))
+        return (-1);
+    if (read(0, &c, 1) != 1)
+        stop("end of file, help");
+    return ((int)(unsigned char)c);
 }
